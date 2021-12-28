ee423b45c48ff562c136af60c817ccb565a37674
jmor1591 <94567474+jmor1591@users.noreply.github.com>
2021-12-27 18:10:16 -0800

    Add files via upload

---
+++ ChooseYourOwnAdventure.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+public class ChooseYourOwnAdventure
+{
+  public static void main(String[] args) 
+  {
+    boolean a = true;
+    boolean b;
+    
+    b = false;
+    
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
